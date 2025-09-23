e88ae101119e845f2bcf106b8424a29bfd15d158
Rafaelakamexican <r3742127@gmail.com>
2025-09-23 11:42:46 -0700

    i created a game
    new game

---
+++ GameplayMechanicsP3Muro/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+}
